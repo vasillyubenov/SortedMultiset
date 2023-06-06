2fd1111fb46e2288c20d49cc683ffcf2013b5eea
VasetoKaiba <vasillyubenov01@gmail.com>
2023-06-06 21:20:01 +0300

    first commit

---
+++ Task1/CakeFactory/SortedMultiset.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SortedMultiset<T> : IEnumerable<T> where T : IComparable<T>
+{
+    private readonly List<T> elements = new List<T>();
+    private readonly IComparer<T> comparer;
+
+    public SortedMultiset(IComparer<T> comparer = null)
+    {
+        this.comparer = comparer ?? Comparer<T>.Default;
+    }
+
+    public void Add(T item)
+    {
+        int index = elements.BinarySearch(item, comparer);
+        if (index < 0)
+            index = ~index; // Get the index of the next greater element
+        elements.Insert(index, item);
+    }
+
+    public bool Remove(T item)
+    {
+        int index = elements.BinarySearch(item, comparer);
+        if (index >= 0)
+        {
+            elements.RemoveAt(index);
+            return true;
+        }
+        return false;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return elements.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
